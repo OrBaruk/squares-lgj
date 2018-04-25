7e475b5da009ce91cf134a2f0ba11b87bae72eca
OrBaruk <orcansian@gmail.com>
2018-04-24 22:37:02 -0300

    Fix state collision

--- src/squares_lgj/core.cljs
+++ src/squares_lgj/core.cljs
@@ -10,18 +10,18 @@
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
+  ;; (q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; circle color and position.
   {:player {:x 50 :y 100 :vx 0 :vy 0}
    :enemies [{:x 0 :y 0 :vx 5 :vy 0}
              {:x 0 :y 0 :vx 0 :vy 5}]
    :candy (new-candy)
-   :lost false})
+   :lost :nothing})
 
-(def player-color [100 255 255])
-(def candy-color [200 255 255])
-(def enemy-color [50 255 255])
+(def player-color [0 255 0])
+(def candy-color [0 0 255])
+(def enemy-color [255 0 0])
 
 (defn draw-player [player]
   (let [x (:x player)
@@ -48,29 +48,24 @@
    :vy (:vy enemy)})
 
 (defn update-state [state]
-  ;; (js/console.log (:lost state))
   (as-> state $
    (update-in $ [:player] move-square)
    (update-in $ [:enemies] #(map move-square %))
    (assoc-in $ [:lost]
              (cond
                (collision? (:player $) (:candy $)) :candy
-               ;; (reduce #(or (collision? (:player $) %1) %2) true (:enemies $)) :enemy
-               :default :nothing)
-
-
-   )))
+               (reduce #(or (collision? (:player $) %2) %1) false (:enemies $)) :enemy
+               :default :nothing))))
 
 (defn draw-enemy [enemy]
   (apply q/fill enemy-color)
   (q/rect (:x enemy) (:y enemy) 10 10))
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (condp #(= (:lost state) %)
-    :candy (q/background 0)
-    :enemy (q/background 125)
-    :nothing (q/background 255))
+  (condp = (:lost state)
+    :candy (q/background 0 255 255)
+    :enemy (q/background 255 255 0)
+    :nothing (q/background 245 245 245))
   (draw-player (:player state))
   (draw-candy (:candy state))
   (count (map draw-enemy (:enemies state))))
